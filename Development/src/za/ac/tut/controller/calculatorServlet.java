a6e461af57d2f002e5cd1bbb7931cbfd21ade9d8
Tran203 <tshetshe203@gmail.com>
2023-02-19 13:37:08 +0200

    Create calculatorServlet.java

---
+++ Development/src/za/ac/tut/controller/calculatorServlet.java
@@ -0,0 +1,31 @@
+package za.ac.tut.contoller;
+
+import za.ac.tut.model.Calculation;
+import java.io.IOException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.RequestDispatcher;
+
+public class calculatorServelet extends HttpServlet{
+	public doPost(HttpServletRequest request, HttpServletResponse response) throws IOException,ServletException{
+		//get values entered by the user
+		Integer num1 = Integer.parseInt(request.getParameter("num1"));
+		Integer num2 = Integer.parseInt(request.getParameter("num2"));
+		//cal the method to perform the calculations
+		Calculation cal = new Calculation();
+		Integer result = cal.Add(num1,num2);
+		
+		//set Attributes *make it easy to use them again
+		request.setAtrribute("num1", num1);
+		request.setAtrribute("num2", num2);
+		request.setAtrribute("result", result);
+		
+		//allow data to  be display back to the user *RequestDispatcher
+		RequestDispatcher disp = request.getRequestDispatcher("output_values.jsp");
+		//forward
+		disp.forward(request,response);
+		
+	}
+}
