417e1a4b97494d17246ef740add85ffb1dad9ca9
Tran203 <tshetshe203@gmail.com>
2023-02-19 14:02:57 +0200

    update

--- Development/src/za/ac/tut/controller/calculatorServlet.java
+++ Development/src/za/ac/tut/controller/calculatorServlet.java
@@ -1,4 +1,4 @@
-package za.ac.tut.contoller;
+package za.ac.tut.controller;
 
 import za.ac.tut.model.Calculation;
 import java.io.IOException;
@@ -8,8 +8,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 import javax.servlet.RequestDispatcher;
 
-public class calculatorServelet extends HttpServlet{
-	public doPost(HttpServletRequest request, HttpServletResponse response) throws IOException,ServletException{
+public class calculatorServlet extends HttpServlet{
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException,ServletException{
 		//get values entered by the user
 		Integer num1 = Integer.parseInt(request.getParameter("num1"));
 		Integer num2 = Integer.parseInt(request.getParameter("num2"));
@@ -18,14 +18,13 @@ public class calculatorServelet extends HttpServlet{
 		Integer result = cal.Add(num1,num2);
 		
 		//set Attributes *make it easy to use them again
-		request.setAtrribute("num1", num1);
-		request.setAtrribute("num2", num2);
-		request.setAtrribute("result", result);
+		request.setAttribute("num1", num1);
+		request.setAttribute("num2", num2);
+		request.setAttribute("result", result);
 		
 		//allow data to  be display back to the user *RequestDispatcher
 		RequestDispatcher disp = request.getRequestDispatcher("output_values.jsp");
 		//forward
-		disp.forward(request,response);
-		
+		disp.forward(request,response);	
 	}
 }
