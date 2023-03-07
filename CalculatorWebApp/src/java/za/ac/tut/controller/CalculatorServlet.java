5cc068636060db0e015c0d8c22414a08f609754a
Tran203 <tshetshe203@gmail.com>
2023-03-07 18:10:10 +0200

    Update CalculatorServlet.java

--- CalculatorWebApp/src/java/za/ac/tut/controller/CalculatorServlet.java
+++ CalculatorWebApp/src/java/za/ac/tut/controller/CalculatorServlet.java
@@ -8,12 +8,15 @@ package za.ac.tut.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import za.ac.tut.model.ArithmeticOperator;
+import za.ac.tut.model.ArithmeticOperatorException;
 
 /**
  *
@@ -23,17 +26,29 @@ public class CalculatorServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        //get values from the user
-        Integer num1 = Integer.parseInt(request.getParameter("num1"));
-        Integer num2 = Integer.parseInt(request.getParameter("num2"));
-        Character op = (request.getParameter("operator")).charAt(0);
-        
-        //Perform Arithmetic operations
-        ArithmeticOperator ap = new ArithmeticOperator();
-        String result = ap.getResult(num1,num2,op);
-        
-        //RequestDispatcher
-        RequestDispatcher disp = request.getRequestDispatcher("output.jsp");
-        disp.forward(request, response);
+        try {
+            //get values from the user
+            Integer num1 = Integer.parseInt(request.getParameter("num1"));
+            Integer num2 = Integer.parseInt(request.getParameter("num2"));
+            Character op = (request.getParameter("operator")).charAt(0);
+            
+            //Perform Arithmetic operations
+            ArithmeticOperator ap = new ArithmeticOperator();
+            
+            String result = ap.getResult(num1,num2,op);
+            
+            //pass results back to user
+            request.setAttribute("num1", num1);
+            request.setAttribute("num2", num2);
+            request.setAttribute("op", op);
+            request.setAttribute("result", result);
+            
+            
+            //RequestDispatcher
+            RequestDispatcher disp = request.getRequestDispatcher("output.jsp");
+            disp.forward(request, response);
+        } catch (ArithmeticOperatorException ex) {
+            Logger.getLogger(CalculatorServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
